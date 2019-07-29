7f734dcc7545e42f4a8e32c0bd714f4cfcc01e29
Kenneth Phang <bunnyppl@gmail.com>
2019-07-29 22:48:43 +0800

    failed test

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -34,6 +34,7 @@ public class MyStringStackTest extends TestCase {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
+		stack.push (s2);
         assertTrue (stack.isEmpty());
         stack.push (s1);
         stack.push (s2);
