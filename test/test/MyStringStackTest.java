a96626a7e8ecb5303abb9b690ed41c84439fd115
Kenneth Phang <bunnyppl@gmail.com>
2019-08-27 16:08:32 +0800

    fix the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -33,7 +33,6 @@ public class MyStringStackTest extends TestCase {
 	public void testPush() {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
-		stack.push (s1);
 		assertTrue (stack.isEmpty());
         stack.push (s1);
         stack.push (s2);
