3067c2037c46eacdb63630aaf21296fc737662ff
Kenneth Phang <bunnyppl@hotmail.com>
2020-04-01 11:39:48 +0800

    fix

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -33,8 +33,9 @@ public class MyStringStackTest extends TestCase {
 	public void testPush() {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
-                stack.push(s1);
 		assertTrue (stack.isEmpty());
+ 		stack.push (s1);
+        	stack.push (s2);
         	assertFalse (stack.isEmpty());
 	}
 
